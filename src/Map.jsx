e04649e39c7d502f611267e8ac83c01c3d7004d9
Tithi1925 <81310263+Tithi1925@users.noreply.github.com>
2024-06-26 16:03:12 +0530

    Update Map.jsx

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap, Tooltip } from 'react-leaflet';
+import React, { useState, useEffect, useCallback } from 'react';
+import { MapContainer, TileLayer, Marker, Polyline, useMap, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import './Map.css'
+import './Map.css';
 import L from 'leaflet';
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { getDatabase, ref, set } from "firebase/database";
+import { initializeApp } from "firebase/app";
+
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import { toast } from 'react-toastify';
 
 const redIconUrl = 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png';
 const greenIconUrl = 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png';
@@ -43,7 +47,7 @@ const ChangeMapCenter = ({ center, shouldCenter, setShouldCenter }) => {
   useEffect(() => {
     if (shouldCenter) {
       map.setView(center, map.getZoom());
-      setShouldCenter(false); // Reset the shouldCenter flag
+      setShouldCenter(false);
     }
   }, [center, shouldCenter, setShouldCenter, map]);
 
@@ -51,11 +55,10 @@ const ChangeMapCenter = ({ center, shouldCenter, setShouldCenter }) => {
 };
 
 const SimpleMap = () => {
-  const start = [23.013487532235562, 72.50403242503077]; // Starting point
+  const start = [23.24116518297178, 72.49185132396424]; // Starting point
   const end = [23.164423073637163, 72.8092796894774]; // Ending point
-  // 23.0132668, 72.5042391
-  const [currentLocation, setCurrentLocation] = useState(); // Initial current location
-  const [mapCenter, setMapCenter] = useState(); // Initial map center
+  const [currentLocation, setCurrentLocation] = useState(null); // Initial current location
+  const [mapCenter, setMapCenter] = useState(start); // Initial map center
   const [shouldCenter, setShouldCenter] = useState(false); // State to track if we should center the map
   const [completedPath, setCompletedPath] = useState([]);
   const [remainingPath, setRemainingPath] = useState([]);
@@ -63,77 +66,66 @@ const SimpleMap = () => {
   const [startAddress, setStartAddress] = useState('');
   const [endAddress, setEndAddress] = useState('');
   const [currentLocationAddress, setCurrentLocationAddress] = useState('');
+  const [username, setUsername] = useState(''); // Add state for username
+  const [isTracking, setIsTracking] = useState(false); // State to track if we are tracking the user
+  const [watchId, setWatchId] = useState(null); // State to track geolocation watch ID
   const provider = new OpenStreetMapProvider();
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setCurrentLocation([latitude, longitude]);
-          setMapCenter([latitude, longitude]); // Center map on the current location
-        },
-        (error) => {
-          console.error('Error getting current position:', error);
-        }
-      );
-    } else {
-      console.log("Geolocation is not available in your browser.");
+  const firebaseConfig = {
+    apiKey: "AIzaSyDeouxYk3mphLe01gwWAKhuRWQUW4Q7j7U",
+    authDomain: "leaftlet-map.firebaseapp.com",
+    projectId: "leaftlet-map",
+    storageBucket: "leaftlet-map.appspot.com",
+    messagingSenderId: "682688095140",
+    appId: "1:682688095140:web:706b3839f8fd0f989f9731",
+    measurementId: "G-YJZ2NFY9ZG"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+
+  const fetchAddress = useCallback(async (lat, lng, setAddressFunc) => {
+    try {
+      const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`);
+      if (response.data && response.data.display_name) {
+        setAddressFunc(response.data.display_name);
+      }
+    } catch (error) {
+      console.error('Error fetching address:', error);
     }
   }, []);
 
-  useEffect(() => {
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setCurrentLocation([latitude, longitude]);
-        setMapCenter([latitude, longitude]); // Center map on the current location
-      },
-      (error) => {
-        console.error('Error getting current position:', error);
+  const fetchRoute = useCallback(async () => {
+    try {
+      if (currentLocation) {
+        const response1 = await axios.get(`https://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${currentLocation[1]},${currentLocation[0]}`, {
+          params: {
+            overview: 'full',
+            geometries: 'geojson'
+          }
+        });
+
+        const completedCoordinates = response1.data.routes[0].geometry.coordinates.map(coord => [coord[1], coord[0]]);
+
+        const response2 = await axios.get(`https://router.project-osrm.org/route/v1/driving/${currentLocation[1]},${currentLocation[0]};${end[1]},${end[0]}`, {
+          params: {
+            overview: 'full',
+            geometries: 'geojson'
+          }
+        });
+
+        const remainingCoordinates = response2.data.routes[0].geometry.coordinates.map(coord => [coord[1], coord[0]]);
+
+        setCompletedPath(completedCoordinates);
+        setRemainingPath(remainingCoordinates);
       }
-    );
-
-    return () => navigator.geolocation.clearWatch(watchId);
-  }, []);
-
-  // useEffect(() => {
-  //   const fetchAddress = async (lat, lng, setAddressFunc) => {
-  //     try {
-  //       const apiKey = '5b3ce3597851110001cf6248b3fd28cd24bf42dd9e0417d885bda499'; // Replace with your OpenRoute API key
-  //       const response = await axios.get(`https://api.openrouteservice.org/geocode/reverse?api_key=${apiKey}&point.lon=${lng}&point.lat=${lat}&size=100`);
-  //       if (response.data && response.data.features && response.data.features.length > 0) {
-  //         setAddressFunc(response.data.features[0].properties.label);
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching address:', error);
-  //     }
-  //   };
-  
-  //   if (start) {
-  //     fetchAddress(start[0], start[1], setStartAddress);
-  //   }
-  //   if (end) {
-  //     fetchAddress(end[0], end[1], setEndAddress);
-  //   }
-  //   if (currentLocation) {
-  //     fetchAddress(currentLocation[0], currentLocation[1], setCurrentLocationAddress);
-  //     console.log(currentLocationAddress)
-  //   }
-  // }, [currentLocation]); 
+    } catch (error) {
+      console.error('Error fetching route:', error);
+    }
+  }, [currentLocation]);
 
   useEffect(() => {
-    const fetchAddress = async (lat, lng, setAddressFunc) => {
-      try {
-        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`);
-        if (response.data && response.data.display_name) {
-          setAddressFunc(response.data.display_name);
-        }
-      } catch (error) {
-        console.error('Error fetching address:', error);
-      }
-    };
-  
     if (start) {
       fetchAddress(start[0], start[1], setStartAddress);
     }
@@ -142,99 +134,127 @@ const SimpleMap = () => {
     }
     if (currentLocation) {
       fetchAddress(currentLocation[0], currentLocation[1], setCurrentLocationAddress);
+      fetchRoute();
     }
   }, [currentLocation]);
-  
-  
-  
 
-  useEffect(() => {
-    const fetchRoute = async () => {
-      try {
-        if (currentLocation) {
-          const response1 = await axios.get(`http://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${currentLocation[1]},${currentLocation[0]}`, {
-            params: {
-              overview: 'full',
-              geometries: 'geojson'
-            }
-          });
+  const startGeolocationWatch = () => {
+    if (username && "geolocation" in navigator) {
+      const id = navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation([latitude, longitude]);
+          setMapCenter([latitude, longitude]); // Center map on the current location
 
-          const completedCoordinates = response1.data.routes[0].geometry.coordinates.map(coord => [coord[1], coord[0]]);
-
-          const response2 = await axios.get(`http://router.project-osrm.org/route/v1/driving/${currentLocation[1]},${currentLocation[0]};${end[1]},${end[0]}`, {
-            params: {
-              overview: 'full',
-              geometries: 'geojson'
-            }
-          });
-
-          const remainingCoordinates = response2.data.routes[0].geometry.coordinates.map(coord => [coord[1], coord[0]]);
-
-          setCompletedPath(completedCoordinates);
-          setRemainingPath(remainingCoordinates);
+          // Update Firebase with the new location
+          if (username) {
+            set(ref(database, 'users/' + username), {
+              lat: latitude,
+              lon: longitude
+            });
+          }
+        },
+        (error) => {
+          console.error('Error getting current position:', error);
         }
-      } catch (error) {
-        console.error('Error fetching route:', error);
-      }
-    };
+      );
 
-    fetchRoute();
-  }, [currentLocation]);
+      setWatchId(id); // Store the watch ID
+    }
+  };
+
+  const stopGeolocationWatch = () => {
+    if (watchId !== null) {
+      navigator.geolocation.clearWatch(watchId);
+      setWatchId(null); // Clear the watch ID
+    }
+  };
 
   const handleSearch = async (event) => {
     event.preventDefault();
     const query = event.target.elements.query.value;
     if (query.trim() !== '') {
-      const results = await provider.search({ query });
-      setSearchResults(results);
-      if (results.length > 0) {
-        const { x, y } = results[0];
-        setMapCenter([y, x]);
-        setShouldCenter(true); 
-      }
-      else{
-        toast.error(`This map can't find ${query}`)
+      try {
+        const results = await provider.search({ query });
+        setSearchResults(results);
+        if (results.length > 0) {
+          const { x, y } = results[0];
+          setMapCenter([y, x]);
+          setShouldCenter(true);
+        } else {
+          toast.error(`This map can't find ${query}`);
+        }
+      } catch (error) {
+        toast.error('Error searching location');
       }
     }
   };
 
+  const handleStartTracking = () => {
+    if (username.trim() !== '') {
+      setIsTracking(true);
+      startGeolocationWatch();
+    } else {
+      toast.error('Please enter a username');
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      stopGeolocationWatch(); // Cleanup on component unmount
+    };
+  }, []);
+
   return (
     <div className='container'>
-    <form onSubmit={handleSearch} className="form-container">
-      <input type="text" name="query" placeholder="Search location" className='maptext' />
-      <button type="submit" className='mapsubmit'>Search</button>
-    </form>
-    {currentLocation && (
-      <MapContainer center={mapCenter} zoom={13} className="map" style={{ height: "400px", width: "100%" }}>
-        <ChangeMapCenter center={mapCenter} shouldCenter={shouldCenter} setShouldCenter={setShouldCenter} />
-        <TileLayer
-          className='tiles'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          maxZoom={19}
-        />
-        {completedPath.length > 0 && <Polyline positions={completedPath} color='green' />}
-        {remainingPath.length > 0 && <Polyline positions={remainingPath} color="red" />}
-        <Marker position={start} icon={redIcon}>
-          <Tooltip>{startAddress}</Tooltip>
-        </Marker>
-        <Marker position={end} icon={greenIcon}>
-          <Tooltip>{endAddress}</Tooltip>
-        </Marker>
-        <Marker position={currentLocation}>
-          <Tooltip>{currentLocationAddress}</Tooltip>
-        </Marker>
-  
-        {searchResults.map((result, index) => (
-          <Marker key={index} position={[result.y, result.x]}>
-            <Tooltip>{result.label}</Tooltip>
-          </Marker>
-        ))}
-      </MapContainer>
-    )}
-  </div>
-  
+  {!isTracking ? (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={handleStartTracking}>Start Tracking</button>
+        </div>
+      ) : (
+        <>
+          <form onSubmit={handleSearch} className="form-container">
+            <input type="text" name="query" placeholder="Search location" className='form-search-container' />
+            <button type="submit" className='mapsubmit'>Search</button>
+          </form>
+          
+            {currentLocation && (
+            <MapContainer center={mapCenter} zoom={8} className="map">
+              <ChangeMapCenter center={mapCenter} shouldCenter={shouldCenter} setShouldCenter={setShouldCenter} />
+              <TileLayer
+                className='tiles'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                maxZoom={19}
+              />
+              {completedPath.length > 0 && <Polyline positions={completedPath} color='green' />}
+              {remainingPath.length > 0 && <Polyline positions={remainingPath} color="red" />}
+              <Marker position={start} icon={redIcon}>
+                <Tooltip>{startAddress}</Tooltip>
+              </Marker>
+              <Marker position={end} icon={greenIcon}>
+                <Tooltip>{endAddress}</Tooltip>
+              </Marker>
+              <Marker position={currentLocation}>
+                <Tooltip>{currentLocationAddress}</Tooltip>
+              </Marker>
+
+              {searchResults.map((result, index) => (
+                <Marker key={index} position={[result.y, result.x]}>
+                  <Tooltip>{result.label}</Tooltip>
+                </Marker>
+              ))}
+            </MapContainer>
+          )}
+        </>
+      )}
+    </div>
   );
 };
 
 export default SimpleMap;
-

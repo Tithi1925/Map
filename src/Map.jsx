3fbe23087e30c7a5341930af432fa4c28a03c503
Tithi1925 <81310263+Tithi1925@users.noreply.github.com>
2024-07-05 16:42:16 +0530

    Update Map.jsx

--- src/Map.jsx
+++ src/Map.jsx
@@ -55,7 +55,7 @@ const ChangeMapCenter = ({ center, shouldCenter, setShouldCenter }) => {
 };
 
 const SimpleMap = () => {
-  const start = [23.24116518297178, 72.49185132396424]; // Starting point
+  const start = [23.013329578801514, 72.5042041101487]; // Starting point
   const end = [23.164423073637163, 72.8092796894774]; // Ending point
   const [currentLocation, setCurrentLocation] = useState(null); // Initial current location
   const [mapCenter, setMapCenter] = useState(start); // Initial map center
